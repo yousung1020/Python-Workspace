43c7592540b4b5ffbdb088205c0312846339df22
yousung1020 <choiyousung1020@gmail.com>
2023-03-06 10:55:22 +0900

    [fix] selenium_practice.py

--- py-study/selenium/selenium_practice.py
+++ py-study/selenium/selenium_practice.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,23 +16,26 @@ browser.get(url)
 id = browser.find_element(By.XPATH, '//*[@id="user_id"]')
 id.click()
 id.send_keys("") # 아이디 입력
-time.sleep(1)
 
 # pw = browser.find_element(By.CSS_SELECTOR, '#user_password')
 pw = browser.find_element(By.XPATH, '//*[@id="user_password"]')
 pw.click()
 pw.send_keys("") # 비번 입력
-time.sleep(1)
 
 login_btn = browser.find_element(By.CSS_SELECTOR, '#loginFrm > div.btn_area > a')
 login_btn.click()
 
-browser.get('https://doit.dongyang.ac.kr/career/')
+browser.get('https://doit.dongyang.ac.kr/Community/Program/ProgramList.aspx')
 
 p_s = browser.page_source
 soup = BeautifulSoup(p_s, "lxml")
-p = soup.find_all('div', attrs={'class':'tit1'})
-print("----------------------\n" + str(p) + "\n----------------------")
+# print(soup)
+p = soup.find_all('td', attrs={'class':'ta'})
+
+# with open("html info.txt", "w+", encoding="utf-8") as a :
+#     a.write(str(p[1].get_text()) + "\n")
+
+print("----------------------\n" + str(p[1].get_text()) + "\n----------------------")
 
 while True:
     pass
